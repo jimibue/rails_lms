70a61be111e55ebbb26566c470f294271f6f2d15
James Yeates <jimibue@gmail.com>
2020-05-21 14:53:12 -0600

    demo done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "courses#index"
 
   resources :users
-  resources :courses
+  resources :courses do
+    resources :enrollments, only: [:index, :new, :create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
